cd4ac43ed0c7080025810dc24fb48ddba42fdd66
Janyl Jumadinova <jjumadinova@allegheny.edu>
2023-11-17 09:25:41 -0500

    Class work from 11/17

--- src/main.py
+++ src/main.py
@@ -2,15 +2,37 @@ import json
 
 # Import libraries
 from CoalStation import CoalGenerator
+from SolarField import SolarPanel
 
-# TODO: Write charge_battery function to store power generated
-#       in .battery file
+def charge_battery(energy: dict = {}):
+    """Charge_battery function to store power generated
+          in .battery file"""
+    with open(".battery", "r") as file:
+        data = json.load(file)
+
+    # Aggregate generated energy
+    data["coal"] += energy["coal"]
+
+    # Write data to json
+    with open(".battery", "w") as file:
+        json.dump(data, file)   
 
 def main():
     
     coal_plant = CoalGenerator()
     coal_plant.use()
     print(coal_plant.energy)
+
+    solar_panel1 = SolarPanel()
+
+    # Charge battery
+    charge_battery(
+        {
+            "solar": solar_panel1.power, 
+            "coal": coal_plant.energy
+        }
+    )
+
     # TODO: Create power generation objects and run them
 
     # TODO: Call charge_battery to store the energy created
